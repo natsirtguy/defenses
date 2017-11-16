c77a0c1538d3fa80da6106e239ff39a26b6e2bc6
Tristan James McKinney <natsirtguy@gmail.com>
2017-11-15 16:53:13 -0800

    Complete the list processing.

--- odds.el
+++ odds.el
@@ -29,7 +29,7 @@
     (dolist (defense (sort (odds-teams s1 s2) 'odds-compare)))))
 
 (defun odds-compare (d1 d2)
-  "Compare two defenses D1 and D2 and return the better one according to Vegas."
+  "Return the better defense of D1 and D2 according to Vegas."
   (if (> (cadr d1) (cadr d2))
       d2
     d1))
@@ -37,7 +37,7 @@
 (defun odds-teams (s1 s2)
   "Return list with elements (name score-vegas score-538) from Vegas in S1 and 538 in S2."
   (let (defenses)
-    (dolist (defense s1 defenses)
+    (while (cdr s1)
       (let* ((team1 (pop s1))
 	     (team2 (pop s1))
 	     (winner (odds-winner team1 team2))
@@ -45,15 +45,21 @@
 	     (ou (string-to-number (cadr loser)))
 	     (spread (string-to-number (cadr (cdr winner))))
 	     (spread-538 (odds-spread-538 winner loser s2)))
-	(cons
-	 (list (car winner)
-	       (/ (+ ou spread) 2.0)
-	       (/ (+ ou spread-538) 2.0))
-	 defenses)))))
+	(setq defenses
+	      (cons (list (car winner)
+		     (/ (+ ou spread) 2.0)
+		     (/ (+ ou spread-538) 2.0))
+		    defenses))))
+    defenses))
 
 (defun odds-spread-538 (winner loser s2)
   "Return spread from 538 given Vegas WINNER and LOSER, and parsed list of teams S2."
-  )
+  (let (spread)
+    (dolist (team s2 spread)
+      (if (and (cadr team) (string= (car team) (car winner)))
+	  (setq spread (string-to-number (cadr team))))
+      (if (and (cadr team) (string= (car team) (car loser)))
+	  (setq spread (- (string-to-number (cadr team))))))))
 
 (defun odds-winner (team1 team2)
   "Return TEAM1 if Vegas thinks they will win, TEAM2 otherwise."
@@ -62,7 +68,7 @@
     team2))
 
 (defun odds-loser (team1 team2)
-  "Return TEAM1 if Vegas thinks they will win, TEAM2 otherwise."
+  "Return TEAM1 if Vegas thinks they will lose, TEAM2 otherwise."
   (if (cadr (cdr team1))		;Only the winning team has a spread.
       team2
     team1))
--- test/odds-test.el
+++ test/odds-test.el
@@ -27,11 +27,14 @@
 	      ("Watsons" nil "-7")
 	      ("Rogers" nil "PK")
 	      ("Cant" "50" nil)))
-	(s2 '(("Cant" nil)
-	      ("Rogers" "-1")
+	(s2 '(("Cant" "-1")
+	      ("Rogers" nil)
 	      ("Hobos" nil)
 	      ("Watsons" "-6.5"))))
-    ))
+    (should
+     (equal
+      (odds-teams s1 s2)
+      '(("Rogers" 25.0 25.5) ("Watsons" 18.5 18.75))))))
 
 (ert-deftest odds-winner-test ()
   "Compare defenses."
@@ -41,6 +44,19 @@
 	(team4 '("Cant" "50" nil)))
     (should (equal (odds-winner team1 team2) team2))
     (should (equal (odds-winner team3 team4) team3))))
+
+(ert-deftest odds-spread-538-test ()
+  "Get the spread from 538."
+  (let ((winner1 '("Watsons" nil "-7"))
+	(loser1 '("Hobos" "44" nil))
+	(winner2 '("Cant" nil "-2"))
+	(loser2 '("Rogers" "43.5" nil))
+	(s2 '(("Cant" nil)
+	      ("Rogers" "-1")
+	      ("Hobos" nil)
+	      ("Watsons" "-6.5"))))
+    (should (= (odds-spread-538 winner1 loser1 s2) -6.5))
+    (should (= (odds-spread-538 winner2 loser2 s2) 1))))
 
 (provide 'odds-test)
 ;;; odds-test.el ends here
